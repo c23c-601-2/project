da11ae4bba2923ea87fcf2a3b00387d68f665f9b
nicefriend123 <dlskarbv@gmail.com>
2024-01-30 16:04:32 +0900

    1604

---
+++ c23c_601_2/src/main/java/com/c23c_601_2/webNK/NewsBoard.java
@@ -0,0 +1,26 @@
+package com.c23c_601_2.webNK;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/newsBoard")
+public class NewsBoard extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public NewsBoard() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
