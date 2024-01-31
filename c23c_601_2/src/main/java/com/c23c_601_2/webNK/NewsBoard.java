a173099706bd8ea94bc9fd2b539f324c2cca437e
nicefriend123 <dlskarbv@gmail.com>
2024-01-31 14:46:05 +0900

    기모리

--- c23c_601_2/src/main/java/com/c23c_601_2/webNK/NewsBoard.java
+++ c23c_601_2/src/main/java/com/c23c_601_2/webNK/NewsBoard.java
@@ -1,13 +1,15 @@
 package com.c23c_601_2.webNK;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/newsBoard")
+@WebServlet("/itboard/newsBoard")
 public class NewsBoard extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -16,6 +18,9 @@ public class NewsBoard extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/itboard/newsBoard.jsp");
+		rd.forward(request, response);
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
