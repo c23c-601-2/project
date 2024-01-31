a7627accef20ebafe80ec7c38dcaaf554c45c643
seonwoopark <tjsdn3823@gmail.com>
2024-01-31 15:15:38 +0900

    회원가입 수정

--- c23c_601_2/src/main/java/com/c23c_601_2/web/Join.java
+++ c23c_601_2/src/main/java/com/c23c_601_2/web/Join.java
@@ -1,11 +1,6 @@
 package com.c23c_601_2.web;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,9 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 
 @WebServlet("/join")
 public class Join extends HttpServlet {
