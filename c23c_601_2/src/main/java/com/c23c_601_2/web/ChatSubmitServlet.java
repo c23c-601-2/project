36c98e3447a8a9c5a2f78b2e1a11e73089b856e6
Hb <Hb@DESKTOP-69CGL2E>
2024-02-05 01:59:03 +0900

    hb 2024-02-05 01:58

--- c23c_601_2/src/main/java/com/c23c_601_2/web/ChatSubmitServlet.java
+++ c23c_601_2/src/main/java/com/c23c_601_2/web/ChatSubmitServlet.java
@@ -1,6 +1,8 @@
 package com.c23c_601_2.web;
 
 import java.io.IOException;
+import java.net.URLDecoder;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,8 +25,8 @@ public class ChatSubmitServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html;charset=UTF-8");
-		String chatName = request.getParameter("chatName");
-		String chatContent = request.getParameter("chatContent");
+		String chatName = URLDecoder.decode(request.getParameter("chatName"), "UTF-8");
+		String chatContent = URLDecoder.decode(request.getParameter("chatContent"), "UTF-8");
 		
 		if (chatName == null || chatName.equals("") || chatContent == null || chatContent.equals("")) {
 			response.getWriter().write("0");
